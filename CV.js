457d9d618a6c8900f98d529d53aa51c8ef8be37e
p1la4r-8 <m.pilar.gomez.30@gmail.com>
2023-02-27 19:24:02 -0300

    Ticmas CV
    primer intento de commit

---
+++ CV.js
@@ -0,0 +1,21 @@
+<script>
+const boton = document.querySelector('#boton');
+boton.addEventListener('click', function() {
+  alert('¡Haz hecho clic en el botón!')}
+  &rbrace;);
+
+  const puesto = document.getElementById("puesto");
+const estudio = document.getElementById("estudio");
+
+puesto.addEventListener("click", () =&gt; {
+    alert("Haz clic en la tarjeta de puesto")});
+
+estudio.addEventListener("mouseenter", () =&gt; {
+    estudio.style.backgroundColor = "#eee"}
+&rbrace;);
+
+estudio.addEventListener("mouseleave", () =&gt; {
+    estudio.style.backgroundColor = "#fff"}
+&rbrace;);
+</script>
+
